028d6e61531b420ab1c915f9620039e1c6666048
Nicolás <ncuervo_175@cue.edu.co>
2026-05-21 21:42:28 -0500

    GREEN: Implementa lógica para Req 4 (No duplicar nota)

--- src/sistema_de_notas.py
+++ src/sistema_de_notas.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from typing import Dict
-from src.exceptions import NotaInvalidaError
+from src.exceptions import NotaInvalidaError, NotaDuplicadaError
 
 class SistemaDeNotas:
     def __init__(self) -> None:
@@ -9,7 +9,12 @@ class SistemaDeNotas:
     def registrar_nota(self, estudiante: str, materia: str, semestre: str, nota: float) -> None:
         if not (0.0 <= nota <= 5.0):
             raise NotaInvalidaError("La nota debe estar entre 0.0 y 5.0")
+        
+        if semestre in self.notas[estudiante][materia]:
+            raise NotaDuplicadaError(f"Ya existe una nota registrada para {materia} en el semestre {semestre}")
+            
         self.notas[estudiante][materia][semestre] = nota
+
 
     def obtener_notas(self, estudiante: str) -> Dict[str, Dict[str, float]]:
         return self.notas[estudiante]
