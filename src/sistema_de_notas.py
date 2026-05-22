826d2d9cdc641a3719e999ceab65bbe4a38b80d9
Nicolás <ncuervo_175@cue.edu.co>
2026-05-21 21:36:01 -0500

    GREEN: Implementa lógica mínima para Req 1

--- src/sistema_de_notas.py
+++ src/sistema_de_notas.py
@@ -1,6 +1,15 @@
+from collections import defaultdict
+from src.exceptions import NotaInvalidaError
+
 class SistemaDeNotas:
+    def __init__(self):
+        self.notas = defaultdict(lambda: defaultdict(dict))
+
     def registrar_nota(self, estudiante, materia, semestre, nota):
-        pass
+        if not (0.0 <= nota <= 5.0):
+            raise NotaInvalidaError("La nota debe estar entre 0.0 y 5.0")
+        self.notas[estudiante][materia][semestre] = nota
 
     def obtener_notas(self, estudiante):
-        return {}
+        return self.notas[estudiante]
+
