d2a5fd049cbcff99b8a56cbfafb8f6100a0f4289
Nicolás <ncuervo_175@cue.edu.co>
2026-05-21 21:36:46 -0500

    REFACTOR: Agrega type hints para Req 1

--- src/sistema_de_notas.py
+++ src/sistema_de_notas.py
@@ -1,15 +1,17 @@
 from collections import defaultdict
+from typing import Dict
 from src.exceptions import NotaInvalidaError
 
 class SistemaDeNotas:
-    def __init__(self):
-        self.notas = defaultdict(lambda: defaultdict(dict))
+    def __init__(self) -> None:
+        self.notas: Dict[str, Dict[str, Dict[str, float]]] = defaultdict(lambda: defaultdict(dict))
 
-    def registrar_nota(self, estudiante, materia, semestre, nota):
+    def registrar_nota(self, estudiante: str, materia: str, semestre: str, nota: float) -> None:
         if not (0.0 <= nota <= 5.0):
             raise NotaInvalidaError("La nota debe estar entre 0.0 y 5.0")
         self.notas[estudiante][materia][semestre] = nota
 
-    def obtener_notas(self, estudiante):
+    def obtener_notas(self, estudiante: str) -> Dict[str, Dict[str, float]]:
         return self.notas[estudiante]
+
 
